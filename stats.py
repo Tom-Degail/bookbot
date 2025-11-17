5c30a9ad92503ed5abf39fc9f9ceb9bbd8d52731
Degail <tom.degail@etu.u-bordeaux.fr>
2025-11-17 13:55:00 +0100

    dict to list

--- stats.py
+++ stats.py
@@ -4,12 +4,18 @@ def wordCount(text):
 
 def charCount(text):
     textLowered = text.lower()
-    dict = {} 
+    dict ={} 
     for c in textLowered:
        if dict.get(c) == None:
         dict[c] = 1
        else:
         dict[c] +=1
     return dict     
+
+def dictSorted(dict):
+    array = []
+    for i in dict:
+        array.append({"name" : i, "num" : dict[i]})
+    return array
 
             
